836b6675d36703a52ee2ae45bdce7ada649b832c
lgcholoquinga <luisg.choloquingal@gamil.com>
2023-07-13 20:02:50 -0500

    workspace

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardLayoutComponent } from './pages/dashboard-layout/dashboard-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardRoutingModule {}
